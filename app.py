224b8995bda4c69c1066bede863385bc3ca89225
PRAKARN KIDNGUN <51776670+mewprakarn@users.noreply.github.com>
2022-04-29 23:32:22 +0700

    Add Web templates

--- app.py
+++ app.py
@@ -19,13 +19,13 @@ import pandas as pd
 # articles' description
 
 
-overall_data = pd.read_csv('static/data/overall_annual.csv')
-account_dict = {key:"{:,}".format(overall_data[overall_data.channel == key]['annual_accounts'].values[0]) for key in overall_data.channel}
-update_date = pd.to_datetime(overall_data['latest_created_at'].max(),format='%Y-%m-%d %H:%M:%S').strftime('%d%b%Y')
+# overall_data = pd.read_csv('static/data/overall_annual.csv')
+# account_dict = {key:"{:,}".format(overall_data[overall_data.channel == key]['annual_accounts'].values[0]) for key in overall_data.channel}
+# update_date = pd.to_datetime(overall_data['latest_created_at'].max(),format='%Y-%m-%d %H:%M:%S').strftime('%d%b%Y')
 
-f = open('static/data/Streamgraph.json',)
-stream_data = json.load(f)
-f.close()
+# f = open('static/data/Streamgraph.json',)
+# stream_data = json.load(f)
+# f.close()
 
 ##########################################################################################################################################
 # Flask Set-Up
@@ -35,21 +35,22 @@ app = Flask(__name__) # Set name of the app.
 app.config['SECRET_KEY'] = 'mykey'
 
 #*Render Template
-@app.route('/')
-def home():
-    return render_template("home.html",account_dict=account_dict,update_date=update_date)
+# @app.route('/')
+# def home():
+#     return render_template("home.html",account_dict=account_dict,update_date=update_date)
 
-@app.route('/ranking')
 def ranking_page():
-    return render_template("ranking.html",ranking_spec=ranking_data)
+# @app.route('/ranking')
+# def ranking_page():
+#     return render_template("ranking.html",ranking_spec=ranking_data)
 
-@app.route('/engagement')
-def engagement_page():
-    return render_template("engagement_v2.html",heatmap_spec1=heatmap1_data,heatmap_spec2=heatmap2_data)
+# @app.route('/engagement')
+# def engagement_page():
+#     return render_template("engagement_v2.html",heatmap_spec1=heatmap1_data,heatmap_spec2=heatmap2_data)
 
-@app.route('/category')
-def category_page():
-    return render_template("category_v2.html",scatter_spec=scatter_data,stream_spec=stream_data)
+# @app.route('/category')
+# def category_page():
+#     return render_template("category_v2.html",scatter_spec=scatter_data,stream_spec=stream_data)
 
 if __name__=='__main__':
     app.run()
