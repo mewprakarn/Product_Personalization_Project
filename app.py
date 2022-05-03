c2a124d746d2bab8f717d2fba2935ffe85c81f1e
PRAKARN KIDNGUN <51776670+mewprakarn@users.noreply.github.com>
2022-05-03 14:49:51 +0700

    Update WebTemplate

--- app.py
+++ app.py
@@ -13,19 +13,19 @@ import pandas as pd
 # Load data into DataFrame
 ##########################################################################################################################################
 
-### Required Data
-# item-item recommend list
-# user-item recommend list
-# articles' description
 
+customers = pd.read_csv('static/model/customers.csv')
+customer_list = dict(zip(customers['customer_id'],customers['name']))
 
-# overall_data = pd.read_csv('static/data/overall_annual.csv')
-# account_dict = {key:"{:,}".format(overall_data[overall_data.channel == key]['annual_accounts'].values[0]) for key in overall_data.channel}
-# update_date = pd.to_datetime(overall_data['latest_created_at'].max(),format='%Y-%m-%d %H:%M:%S').strftime('%d%b%Y')
+prediction = pd.read_csv('static/model/prediction.csv')
+prediction['prediction'] = prediction['prediction'].apply(lambda x: x.split())
+prediction['recent_purchase'] = prediction['recent_purchase'].apply(lambda x: x.split())
 
-# f = open('static/data/Streamgraph.json',)
-# stream_data = json.load(f)
-# f.close()
+temp_pred = prediction['prediction'][0]
+temp_recent = prediction['recent_purchase'][0]
+
+articles = pd.read_csv('static/model/articles.csv')
+
 
 ##########################################################################################################################################
 # Flask Set-Up
@@ -35,21 +35,17 @@ app = Flask(__name__) # Set name of the app.
 app.config['SECRET_KEY'] = 'mykey'
 
 #*Render Template
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
-    return render_template("home.html")
+    return render_template("home.html",nameDict=customer_list,rec_items=temp_pred,last_items=temp_recent,articles=articles)
 
-# @app.route('/ranking')
-# def ranking_page():
-#     return render_template("ranking.html",ranking_spec=ranking_data)
+@app.route('/', methods=['POST'])  
+def updatehome():     
+    select = request.form.get('customers')
+    temp_pred2 = prediction['prediction'][prediction.name == select]
+    temp_recent2 = prediction['recent_purchase'][prediction.name == select]
+    return render_template("home.html",nameDict=customer_list,rec_items=temp_pred2,last_items=temp_recent2,articles=articles)
 
-# @app.route('/engagement')
-# def engagement_page():
-#     return render_template("engagement_v2.html",heatmap_spec1=heatmap1_data,heatmap_spec2=heatmap2_data)
-
-# @app.route('/category')
-# def category_page():
-#     return render_template("category_v2.html",scatter_spec=scatter_data,stream_spec=stream_data)
 
 if __name__=='__main__':
     app.run()
